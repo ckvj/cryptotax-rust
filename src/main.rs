13d3673290a16140c2319da551fc2340b7d902e4
ckvj <carlkvogel@gmail.com>
2023-06-20 13:06:03 -0400

    Updated Config

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
 use std::env;
 mod config;
-
+mod import_trades;
 
 fn main() {
 
     let args: Vec<String> = env::args().collect();
+    
     let config_filepath: String = String::from(&args[1]);
 
     let config = config::build_config(&config_filepath);
     println!("{:?}", config);
+
+    // let trades = import_trades::import_trades(&config);
     
 }
 
